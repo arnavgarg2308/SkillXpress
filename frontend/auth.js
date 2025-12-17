ed450225594ba5dce10f27f1d292afa08747f802
Arnav Garg <arnavgarg2308@gmail.com>
2025-12-17 18:28:29 +0530

    sign in,sign up

---
+++ frontend/auth.js
@@ -0,0 +1,9 @@
+// frontend/js/auth.js
+
+const SUPABASE_URL = "https://ltxrynmhihxmmzrnnduw.supabase.co";
+const SUPABASE_KEY = "sb_publishable_fjjEdhlNVgCsfdKb5k_9Hg_zlR0HS4L";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
