1e1e7f2127d91d734a10ebe08f178874e4e60eaf
Arnav Garg <arnavgarg2308@gmail.com>
2025-12-19 10:09:06 +0530

    save data error

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,6 +1,6 @@
 // auth.js
 const SUPABASE_URL = "https://ltxrynmhihxmmzrnnduw.supabase.co";
-const SUPABASE_KEY = "sb_publishable_fjjEdhlNVgCsfdKb5k_9Hg_zlR0HS4L";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0eHJ5bm1oaWh4bW16cm5uZHV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MDAxNTksImV4cCI6MjA4MTQ3NjE1OX0.jpxJbr3by-75XRCY0lC9gDuU3_cLuVk6fYRzWP2f-N0";
 
 window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
