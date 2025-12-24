02c32e04b12e346f411c0d90cdf8f63dd7a7a685
Arnav Garg <arnavgarg2308@gmail.com>
2025-12-24 16:17:23 +0530

    h

--- backend/skillback.js
+++ backend/skillback.js
@@ -21,7 +21,8 @@ const corsOptions = {
   origin: [
     "https://skill-xpress.vercel.app",
     "http://localhost:3000",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "https://skillxpress.onrender.com" 
   ],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
